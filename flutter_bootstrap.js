4279488846be7592a11af11ed1a0113af36cec30
Nikolai Dokutovich <Nikolai.Dokutovich@artezio.com>
2025-04-03 18:13:41 +0300

    Reverted to space shooter

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2624408840"
+    serviceWorkerVersion: "68236779"
   }
 });
